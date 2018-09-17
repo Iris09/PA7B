88e374abe5e8a2858204d3b765e0a420ba024998
Iris09 <irisgp42@gmail.com>
2018-09-17 17:04:00 -0500

    September 17th

--- ComputeBMI.java
+++ ComputeBMI.java
@@ -6,18 +6,32 @@ public class ComputeBMI{
 final double KILOGRAMS_PER_POUND= 0.453592;
 final double METERS_PER_INCH =0.0254;
 
-     System.out.println("Enter wieght in pounds:");
+     System.out.println("Enter weight in pounds:");
      double weight =input.nextDouble();
 
      System.out.println("Enter height in inches");
      double height =input.nextDouble();
 
      double weigthInKilograms = weight * KILOGRAMS_PER_POUND;
-     double heightifInMeters = height * METERS_PER_INCH;
+     double heightInMeters = height * METERS_PER_INCH;
+//double bmi =weigthInKilograms / (heightInMeters) *
+//heightInMeters);
+double bmi = weigthInKilograms / Math.pow(heightInMeters , 2);
+if (bmi < 16 )
+  System.out.println ("Your are seriusly underweight");
+  else if (bmi < 18)
+  System.out.println ("Your are underweight");
+  else if (bmi < 24)
+System.out.println("Your are normal weight");
+else if (bmi < 29)
+System.out.println("Your are seriously overweight");
+else if (bmi <35)
+System.out.println("Your are seriusly overweight");
+else
+System.out.println("Your are gravely overweight");
 
+}
 
 
 
   }
-
-}
